b8b0a16a37e3d1ea45bcc174a19debc1ebe01fe6
miarhost <maria.tikhonova@syneforge.com>
2023-09-18 21:27:22 +0300

    Added request tests and cases for uploads endpoints

--- spec/requests/uploads_spec.rb
+++ spec/requests/uploads_spec.rb
@@ -6,13 +6,16 @@ describe 'Uploads', type: :request do
   describe 'Token authorization' do
     context 'unautnorized' do
       include_examples 'v1:unauthorized_request', :post, '/api/v1/uploads/1/upload_file', params: { id: 1, file: 'example_file.png' }
-      include_examples 'v1:unauthorized_request', :post, '/api/v1/uploads/', params: { "upload": {"name": "Test folder"} }
+      include_examples 'v1:unauthorized_request', :post, '/api/v1/uploads/', params: { 'upload': {'name': 'Test folder' } }
+      include_examples 'v1:unauthorized_request', :patch, '/api/v1/uploads/1', params: { 'upload': {'name': 'Updated folder name' } }
+      include_examples 'v1:unauthorized_request', :delete, '/api/v1/uploads/1/remove_file', params: { id: 1 }
     end
   end
 
   let!(:upload) { create(:upload) }
   let!(:user) { FactoryBot.create(:user) }
   let!(:authenticate) { Users::Authentication.call(user.email, user.password) }
+
   describe 'GET /api/v1/uploads/:id/load_prediction_for_infos' do
     let!(:uploads_infos) { create_list(:uploads_info, 3, upload_id: upload.id) }
 
@@ -33,28 +36,136 @@ describe 'Uploads', type: :request do
 
   describe 'POST /api/v1/uploads/:id/upload_file' do
     include_context 'v1:authorized_request'
-    it 'successfully uploads a file' do
-      post "/api/v1/uploads/#{upload.id}/upload_file", params: { id: upload.id, file: fixture_file_upload('example_file.png') },
-          headers: { Authorization: "Bearer #{authenticate}" }
+    context 'successfully creates an attachment' do
+      it 'successfully uploads a file' do
+        post "/api/v1/uploads/#{upload.id}/upload_file", params: { id: upload.id, file: fixture_file_upload('example_file.png') },
+        headers: { Authorization: "Bearer #{authenticate}" }
 
-      expect(response.status).to eq(200)
-      expect(response.body).to eq({ filename: 'example_file.png', upload_id: upload.id }.to_json)
+        expect(response.status).to eq(200)
+        expect(response.body).to eq({ filename: 'example_file.png', upload_id: upload.id }.to_json)
+      end
+    end
+
+    context 'purges attached file with error statement' do
+      it 'when file is empty' do
+        post "/api/v1/uploads/#{upload.id}/upload_file", params: { id: upload.id, file: nil },
+        headers: { Authorization: "Bearer #{authenticate}" }
+
+        expect(response.status).to eq(406)
+        expect(response.body).to include_json(
+          {
+            'status': 'not_acceptable',
+            'message': 'Please add a file'
+          }
+        )
+      end
+
+      it 'when file is unappropriate format' do
+        post "/api/v1/uploads/#{upload.id}/upload_file",
+        params: { id: upload.id, file: fixture_file_upload('text_example.txt') },
+        headers: { Authorization: "Bearer #{authenticate}"}
+
+        expect(response.status).to eq(422)
+        expect(response.body).to include_json(
+          {
+            'status': 'unprocessable_entity',
+            'message': 'File is not a valid file format'
+          }
+        )
+      end
+
+      it 'when file is unappropriate size' do
+        post "/api/v1/uploads/#{upload.id}/upload_file",
+        params: { id: upload.id, file: fixture_file_upload('invalid_size_file_example.jpg') },
+        headers: { Authorization: "Bearer #{authenticate}"}
+
+        expect(response.status).to eq(422)
+        expect(response.body).to include_json(
+          {
+            'status': 'unprocessable_entity',
+            'message': 'File File size should be greater than 2 KB'
+          }
+        )
+      end
     end
   end
 
   describe 'POST /api/v1/uploads' do
-    it 'creates an upload' do
-      post '/api/v1/uploads', params: { "upload": {"name": "Test folder"} },
-          headers: { Authorization: "Bearer #{authenticate}" }
-      expect(response).to have_http_status(:success)
-      expect(response.body).to include_json(
-        {
-          "user_id": user.id,
-          "name": "Test folder",
-          "upload_attachment": nil
-        }
-      )
+    include_context 'v1:authorized_request'
+    context 'successfully creates an upload' do
+      it "returns json with created record's attributes" do
+        post '/api/v1/uploads', params: { 'upload': {'name': 'Test folder'} },
+        headers: { Authorization: "Bearer #{authenticate}" }
+
+        expect(response).to have_http_status(:created)
+        expect(response.body).to include_json(
+          {
+            'user_id': user.id,
+            'name': 'Test folder',
+            'upload_attachment': nil
+          }
+        )
+      end
+    end
+
+    context 'fails to create a record due to validation' do
+      it "doesn't create an upload with a blank name" do
+        post '/api/v1/uploads', params: {'upload': { 'name': ''} },
+        headers: { Authorization: "Bearer #{authenticate}"}
+
+          expect(response).to have_http_status(422)
+          expect(response.body).to include_json(
+            {
+            'status': 'unprocessable_entity',
+            'message': "Name can't be blank"
+            }
+          )
+      end
     end
   end
 
+  describe 'PATCH /api/v1/uploads/:id' do
+    include_context 'v1:authorized_request'
+    context 'successfully updates record' do
+      it 'returns json with updated record attributes' do
+        patch "/api/v1/uploads/#{upload.id}", params: { 'upload': {'name': 'Updated folder name'} },
+        headers: { Authorization: "Bearer #{authenticate}" }
+
+        expect(response.status).to eq(200)
+        expect(response.body).to include_json(
+          {
+            'user_id': upload.user.id,
+            'name': 'Updated folder name',
+            'upload_attachment': nil
+          }
+        )
+      end
+    end
+
+    context 'fails to update record' do
+      it 'returns json with error message' do
+        patch "/api/v1/uploads/#{upload.id}", params: { 'upload': { 'name': '' } },
+        headers: { Authorization: "Bearer #{authenticate}" }
+
+        expect(response.status).to eq(422)
+        expect(response.body).to include_json(
+          {
+            'status': 'unprocessable_entity',
+            'message': "Name can't be blank"
+          }
+        )
+      end
+    end
+  end
+
+  describe 'DELETE /api/v1/uploads/:id/remove_file' do
+    let!(:upload_attachment) { create(:upload_attachment, upload_id: upload.id) }
+    include_context 'v1:authorized_request'
+    it 'successfully removes file attachment' do
+      delete "/api/v1/uploads/#{upload.id}/remove_file", params: { "id": upload.id },
+      headers: { Authorization: "Bearer #{authenticate}" }
+      expect(response.status).to eq(204)
+      expect { upload_attachment.reload }.to raise_exception(ActiveRecord::RecordNotFound)
+    end
+  end
 end
