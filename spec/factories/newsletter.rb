912c257752033c6263b352a99f58ea029251d2d5
miarhost <miarhost@gmail.com>
2024-05-31 15:44:25 +0300

    [migrate] Update newsletters endpoints tests and webmock twilio responses

--- spec/factories/newsletter.rb
+++ spec/factories/newsletter.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
     uploads_info_id { 1 }
     header { 'New Event' }
     name { Faker::Creature::Cat }
+    ad_type { 0 }
   end
 end
--- spec/requests/newsletters_spec.rb
+++ spec/requests/newsletters_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 describe 'Newsletters', type: :request do
+  let!(:topic) { create(:topic)}
   let!(:subscription) { create(:subscription, topic_id: topic.id) }
   let!(:newsletter) { create(:newsletter, subscription_id: subscription.id) }
   let!(:user) { create(:user, subscription_ids: [subscription.id]) }
   let!(:another_user_1) { create(:user, subscription_ids: [subscription.id], phone_number: "+222222222")}
   let!(:another_user_2) { create(:user, subscription_ids: [subscription.id], phone_number: "+333333333")}
-  let!(:topic) { create(:topic)}
 
   describe 'Token authorization' do
     context 'unauthorized' do
@@ -17,18 +17,17 @@ describe 'Newsletters', type: :request do
     include_context 'v1:authorized_request'
     it 'delivers sms on newsletter and gets success response' do
       post "/api/v1/newsletters/#{newsletter.id}/sms_users_newsletter",
-      params: { id: newsletter.id },
       headers: { Authorization: "Bearer #{authenticate}"}
 
       expect(response).to have_http_status(:success)
       expect(response.body).to include_json(
-        UnorderedArray([
+        [
           {
             "status": "queued",
             "body": "Sent from your Twilio trial account - New Event"
           }
         ])
-      )
+
     end
   end
 end
