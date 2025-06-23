24beaf89547d340b38edcf695ded86c5fff1b437
miarhost <miarhost@gmail.com>
2025-06-23 18:26:36 +0300

    [devops] Add worker deployment manifest

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 require 'sidekiq-status'
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/1' }
+  config.redis = { url: 'redis://redis:6379/1' }
   Sidekiq::Status.configure_server_middleware config, expiration: 30.minutes.to_i
   Sidekiq::Status.configure_client_middleware config, expiration: 1.hour.to_i
   config.logger.level = Logger::INFO
 end
 Sidekiq.configure_client do |config|
-  config.redis = { url:  'redis://localhost:6379/1' }
+  config.redis = { url:  'redis://redis:6379/1' }
   Sidekiq::Status.configure_client_middleware config, expiration: 1.hour.to_i
 end
